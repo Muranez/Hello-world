0ed5c2b89b9418caec9bfa9bd1af927d4c1a59d1
Nikita <heee2010@mail.ru>
2015-10-09 12:26:02 +0400

    2.5.1

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "sdt.h"
 
 main(){
-int a,b,c;
+int a,b,c,d;
 cout << "Enter a:";
 cin >> a;
 cout << "Enter b:";
 cin >> b;
 c=a+b;
-cout << "Summa=" << c;
+cout << "Summa=" << c << "\n";
+d=a-b;
+cout << "Raznost=" << d;
 }
